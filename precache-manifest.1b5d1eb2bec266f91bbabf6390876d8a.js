795274cd22814eac7f3129b33e9a76d874717460
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-10-13 16:01:26 +0530

    Updates

---
+++ precache-manifest.1b5d1eb2bec266f91bbabf6390876d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ad1784c3e5bfe3a8ad3fcd8427a3e0",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "e7a5ab2f060d9dc81024",
+    "url": "/myportfolio/static/css/main.7cc96530.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/myportfolio/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/myportfolio/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a5ab2f060d9dc81024",
+    "url": "/myportfolio/static/js/main.f75877b6.chunk.js"
+  },
+  {
+    "revision": "e6a1b055e95a4a385f54",
+    "url": "/myportfolio/static/js/runtime-main.42579bc2.js"
+  }
+]);
